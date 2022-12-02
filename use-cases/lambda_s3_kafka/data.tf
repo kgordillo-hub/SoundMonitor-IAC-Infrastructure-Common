8f80ecaa9b6e33b6f2dd61c4bc1fa35262001d83
kgordillo-hub <ksgordilloo@correo.udistrital.edu.co>
2022-12-02 09:50:06 -0500

    Update data.tf

--- use-cases/lambda_s3_kafka/data.tf
+++ use-cases/lambda_s3_kafka/data.tf
@@ -1,8 +1,8 @@
 data "aws_s3_bucket" "s-1" {
-  bucket = "soundmonitor-audiodata"
+  bucket = "soundmonitor-audiodata-03"
 }
 
 data "aws_s3_bucket" "s-2" {
-  bucket = "soundmonitor-visualdata"
+  bucket = "soundmonitor-visualdata-03"
 }
 
